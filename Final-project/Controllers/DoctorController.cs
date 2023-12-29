75abf5ceb76e83604bb3007d4ae1b661cfdce286
Nahar0912 <anika.viki987@gmail.com>
2023-12-29 21:53:07 +0600

    dr

---
+++ Final-project/Controllers/DoctorController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Final_project.Controllers
+{
+    public class DoctorController : ApiController
+    {
+
+    }
+}
